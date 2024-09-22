056ae186e67876ab9009cafc6a8f21d95141be3c
msh <gtfsolo@gmail.com>
2024-09-22 22:36:24 +0300

    init git flow project

---
+++ script/script.js
@@ -0,0 +1,9 @@
+
+// Необходимо создать функции shortProposal сокращения текста ...
+// $ featured/create-fnc-short
+console.log(shortProposal(str, length));
+
+
+// $ featured/add-fnc-welcome
+console.log(showMessage('welcome'));
+
